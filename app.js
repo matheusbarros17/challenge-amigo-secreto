4ecc2b73ea1e1cf006794469d9440d107419048e
matheusbarros17 <matheuscarvalho_5@hotmail.com>
2025-02-26 20:41:33 -0300

    criado a função adicionarAmigo

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 let amigos = [];
 
+function adicionarAmigo() {
+
+    if (validarEntrada()) {
+
+        let entrada = document.querySelector('input').value;
+        amigos.push(entrada);
+        console.log(amigos);
+        document.querySelector('input').value = "";
+        atualizarLista();
+
+    } else {
+        alert("Por favor insira um nome válido");
+    }
+        
+}
+
 function validarEntrada() {
     let entrada = document.querySelector('input').value;
 
